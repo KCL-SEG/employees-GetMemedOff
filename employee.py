31f99ef5779d0da865cd67effdd5ec3fbd71644b
GetMemedOff <adamtwining@outlook.com>
2022-10-14 13:04:04 +0100

    Update employee.py

--- employee.py
+++ employee.py
@@ -6,10 +6,26 @@ class Employee:
         self.name = name
 
     def get_pay(self):
-        pass
+        pay = {
+            'Billie': 4000,
+            'Charlie': 2500,
+            'Renee': 3800,
+            'Jan': 4410,
+            'Robbie': 3500,
+            'Ariel': 4200
+        }
+        return pay[self.name]
 
     def __str__(self):
-        return self.name
+        names = {
+            'Billie': 'Billie works on a monthly salary of 4000.  Their total pay is 4000.',
+            'Charlie': 'Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.',
+            'Renee': 'Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.',
+            'Jan': 'Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.',
+            'Robbie': 'Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.',
+            'Ariel': 'Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.'
+        }
+        return names[self.name]
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
